6e242f815a857d8e18693a33b76ed495d2214007
siddiquah anjum <50186478+siddiquah@users.noreply.github.com>
2020-12-23 01:17:46 +0530

    Add files via upload

---
+++ Easy6_SelectionSort.java
@@ -0,0 +1,25 @@
+public class Easy6_SelectionSort {
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String args[])
+    {
+        Easy6_SelectionSort ob = new Easy6_SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+}
---
+++ Easy8_isValidSubsequence.java
@@ -0,0 +1,24 @@
+public class Easy8_isValidSubsequence {
+    static boolean isSubSequence(String str1, String str2, int m, int n)
+    {
+        if (m == 0)
+            return true;
+        if (n == 0)
+            return false;
+        if (str1.charAt(m-1) == str2.charAt(n-1))
+            return isSubSequence(str1, str2, m-1, n-1);
+        return isSubSequence(str1, str2, m, n-1);
+    }
+    public static void main (String[] args)
+    {
+        String str1 = "anuj";
+        String str2 = "anjum";
+        int m = str1.length();
+        int n = str2.length();
+        boolean res = isSubSequence(str1, str2, m, n);
+        if(res)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
